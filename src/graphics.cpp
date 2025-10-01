3539a9f1a476e55cdc6afcd0a13058780c0fbddd
Raydelto Hernandez <raydelto@gmail.com>
2025-10-01 14:49:32 -0300

    Fixing buffer overflow

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -5,20 +5,20 @@
 
 void drawPixel(SDL_Surface *surface, int x, int y, unsigned int color)
 {
-	unsigned int offset = SCREEN_WIDTH * y + x;
-	if (offset > TOTAL_PIXELS)
-	{
-		return;
-	}
+    unsigned int offset = SCREEN_WIDTH * y + x;
+    if (offset >= TOTAL_PIXELS)
+    {
+        return;
+    }
 
-	unsigned int *pixels;
-	pixels = (unsigned int *)surface->pixels;
-	pixels[offset] = color;
+    unsigned int *pixels;
+    pixels = (unsigned int *)surface->pixels;
+    pixels[offset] = color;
 }
 
 void clearSreen(SDL_Surface *surface)
 {
-	unsigned int *pixels;
-	pixels = (unsigned int *)surface->pixels;
-	memset(pixels, 0, SCREEN_WIDTH * SCREEN_HEIGHT * 4);
+    unsigned int *pixels;
+    pixels = (unsigned int *)surface->pixels;
+    memset(pixels, 0, SCREEN_WIDTH * SCREEN_HEIGHT * 4);
 }
